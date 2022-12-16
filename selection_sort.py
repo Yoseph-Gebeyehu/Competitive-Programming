892a3ff1dee7bf686b7a5e8ec8d262dba9bc2ede
Yoseph-Gebeyehu <93756919+Yoseph-Gebeyehu@users.noreply.github.com>
2022-12-15 23:27:36 -0800

    Create selection_sort.py

---
+++ selection_sort.py
@@ -0,0 +1,37 @@
+#User function Template for python3
+
+class Solution: 
+    def select(self, arr, i):
+        # code here 
+        for i in range(len(arr)):
+            min_index = i
+            for j in range(i+1, len(arr)):
+                if arr[min_index] > arr[j]:
+                    min_index = j
+            arr[i], arr[min_index] = arr[min_index], arr[i]
+            return ar[min_index]
+    def selectionSort(self, arr,n):
+        #code here
+        for i in range(len(arr)):
+            min_index = i
+            for j in range(i+1, len(arr)):
+                if arr[min_index] > arr[j]:
+                    min_index = j
+            arr[i], arr[min_index] = arr[min_index], arr[i]
+        return arr
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__': 
+    t = int(input())
+    for _ in range(t):
+        n = int(input())
+        arr = list(map(int, input().strip().split()))
+        Solution().selectionSort(arr, n)
+        for i in range(n):
+            print(arr[i],end=" ")
+        print()
+# } Driver Code Ends
