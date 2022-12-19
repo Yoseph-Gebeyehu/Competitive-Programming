f74221787e733f77ba0c0aa05fdd0a314e30463a
Yoseph-Gebeyehu <93756919+Yoseph-Gebeyehu@users.noreply.github.com>
2022-12-19 00:41:15 -0800

    Update targetIndices.py

--- targetIndices.py
+++ targetIndices.py
@@ -1,11 +1,9 @@
 class Solution:
     def targetIndices(self, a: List[int], targetNo: int) -> List[int]:
-        numSwap = 0
         arr = []
         for i in range(len(a) - 1):
             for j in range(len(a) - i -1):
-                if a[j] > a[j+1]:
-                    numSwap += 1      
+                if a[j] > a[j+1]:    
                     a[j], a[j+1] = a[j+1], a[j]
         for i in range(len(a)):
             if a[i] == targetNo:
