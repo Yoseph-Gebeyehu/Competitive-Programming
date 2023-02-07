1e7a8fa18f349fa72dc72f36b23f62ad6424f7d9
Yoseph-Gebeyehu <93756919+Yoseph-Gebeyehu@users.noreply.github.com>
2023-02-07 05:00:05 -0800

    Create move_zeros.py

---
+++ move_zeros.py
@@ -0,0 +1,12 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        j = 0
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                tmp = nums[i]
+                nums[i] = nums[j]
+                nums[j] = tmp
+                j += 1
